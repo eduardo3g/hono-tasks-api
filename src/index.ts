d37f80c8741c74b78118e0c20d385729305b3ace
Eduardo Santana <eduardosbrasil10@gmail.com>
2024-10-21 20:25:02 -0300

    refactor: move handler to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { serve } from "@hono/node-server";
-import { OpenAPIHono } from "@hono/zod-openapi";
 
-const app = new OpenAPIHono();
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+import app from "./app.js";
 
 const port = 3000;
-console.log(`Server is running on port ${port}`);
+// eslint-disable-next-line no-console
+console.log(`Server is running on port http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
